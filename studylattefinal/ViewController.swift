2fd4120ec47cff9b183d05ae00961707e3bcf003
samuelhiggs <38268988+samuelhiggs@users.noreply.github.com>
2018-04-12 15:36:44 -0500

    Add files via upload
    first commit

---
+++ studylattefinal/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  studylattefinal
+//
+//  Created by Samuel Higgs on 4/10/18.
+//  Copyright © 2018 Samuel Higgs. All rights reserved.
+//
+
+import UIKit
+import FacebookLogin
+import FacebookCore
+
+func applicationDidBecomeActive(application: UIApplication) {
+    // Call the 'activate' method to log an app event for use
+    // in analytics and advertising reporting.
+    AppEventsLogger.activate(application)
+    // ...
+}
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        let loginButton = LoginButton(readPermissions: [ .publicProfile ])
+        loginButton.center = view.center
+        
+        view.addSubview(loginButton)
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
