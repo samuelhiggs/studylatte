a72bf5a2a6df6d138f0a601ea8544e558516597e
jwdunlap <idunlap@rocketmail.com>
2018-04-29 17:26:36 -0500

    Added Map View and Table View for initial screen after facebook login

--- studylattefinal/NearbyShopsViewController.swift
+++ studylattefinal/NearbyShopsViewController.swift
@@ -7,15 +7,38 @@
 //
 
 import UIKit
+import MapKit
 
 class NearbyShopsViewController: UIViewController {
+    
+    @IBOutlet weak var MapSegControl: UISegmentedControl!
 
+
+
+    @IBOutlet weak var tableView: UIView!
+    @IBOutlet weak var mapView: MKMapView!
+    var currentOption = 0
+    
     override func viewDidLoad() {
+        self.tableView.isHidden = true
         super.viewDidLoad()
+        
 
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func mapSegControlAction(_ sender: UISegmentedControl) {
+        self.currentOption = self.MapSegControl.selectedSegmentIndex
+        switch MapSegControl.selectedSegmentIndex {
+        case 0:
+            tableView.isHidden = true
+        case 1:
+            tableView.isHidden = false
+        default:
+            break;
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
