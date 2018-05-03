c67e6994c58eed369ce4e5a7ef034189be0a3f05
Samuel Higgs <samman06219@yahoo.com>
2018-05-03 17:18:41 -0500

    add/remove btn
    add/remove btn

--- studylattefinal/DetailsPageViewController.swift
+++ studylattefinal/DetailsPageViewController.swift
@@ -13,50 +13,72 @@ import MapKit
 // Creates array for Favorites list
 var favList:[MKMapItem] = []
 
+
 class DetailsPageViewController: UIViewController {
-    
+    var shop:MKMapItem?
     @IBOutlet weak var shopName: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var isOpenLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var addBtn: UIButton!
+    @IBOutlet weak var removeBtn: UIButton!
+    @IBOutlet weak var isFav: UILabel!
+        
+    @IBAction func removeFromFav(_ sender: Any) {
+        if (favList.contains(shop!)) {
+            favList.remove(at: favList.index(of: shop!)!)
+            removeBtn.isHidden = true
+            addBtn.isHidden = false
+            isFav.isHidden = true
+        }
+    }
     
     @IBAction func appendToFavArray(_ sender: Any) {
         // append the current label and location to favorites list
         if (!favList.contains(shop!)) {
             favList.append(shop!)
+            isFav.isHidden = false
+            addBtn.isHidden = true
+            removeBtn.isHidden = false
             print(favList)
             print("BREAK")
         }
     }
     
-        var shop:MKMapItem?
-    
     override func viewDidLoad() {
-        super.viewDidLoad()        
         
+        removeBtn.isHidden = true
+        isFav.isHidden = true
         populateUI()
+        
+        if (favList.contains(shop!)) {
+            isFav.isHidden = false
+            addBtn.isHidden = true
+            removeBtn.isHidden = false
+        super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    func populateUI() {
-        if let shop = shop {
-            shopName.text = shop.name
-            addressLabel.text = parseAddress(selectedItem: shop.placemark)
-            self.title = shop.name
-            if shop.phoneNumber!.contains("1") {
-                openTable(isOpen:true)
-            } else{
-                print(shop.phoneNumber!)
-                openTable(isOpen:false)
+        
+        func populateUI() {
+            if let shop = shop {
+                shopName.text = shop.name
+                addressLabel.text = parseAddress(selectedItem: shop.placemark)
+                self.title = shop.name
+                if shop.phoneNumber!.contains("1") {
+                    openTable(isOpen:true)
+                } else{
+                    print(shop.phoneNumber!)
+                    openTable(isOpen:false)
+                }
             }
         }
-    }
     
     func openTable(isOpen:Bool){
         if isOpen == true{
